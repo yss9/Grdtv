ad17cb8296bfa24da338c6c64380ed32bae739b9
Darkroom01 <darkroom01@icloud.com>
2024-03-20 20:05:07 +0900

    회원가입 DB 테스트

--- src/main/java/proj/travien/domain/User.java
+++ src/main/java/proj/travien/domain/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,9 +23,16 @@ public class User {
     private String email;
     private String password;
     private String name;
-    private Boolean agencyAuthority;
-    private Integer points;
+    private Boolean agencyAuthority = false;
+    private Integer points = 0;
 
     @OneToMany(mappedBy = "user")
     private Set<Like> likes = new HashSet<>();
+
+    @Builder
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
 }
