2f087c81c57cf0d0a8c1b7bad83fcd6a05cce20d
merona0531 <moonpowr0531@naver.com>
2024-04-01 15:51:04 +0900

    경서 이때까지 한것들

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import ReviewPage from './pages/reviewPage/reviewPage';
+import ScrollPage from './pages/scrollPage/scrollPage';
 import LoginPage from './pages/LoginPage/index';
 
 function App() {
     return (
-        <>
-            {/*초기 화면*/}
-            <LoginPage />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/review" element={<ReviewPage />} />
+                <Route path="/scroll" element={<ScrollPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
