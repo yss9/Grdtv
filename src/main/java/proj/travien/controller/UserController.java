b0784104c46dda53f87acf933302e111eaa3462c
Darkroom01 <darkroom01@icloud.com>
2024-04-13 19:15:46 +0900

    회원가입 로그인 예외처리 및 로그인시 jwt토큰 반환 추가

--- src/main/java/proj/travien/controller/UserController.java
+++ src/main/java/proj/travien/controller/UserController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import proj.travien.JwtUtil;
 import proj.travien.domain.User;
 import proj.travien.dto.UserDTO;
+import proj.travien.exception.EmailAlreadyUsedException;
 import proj.travien.service.UserService;
 
 @RestController
@@ -15,10 +17,17 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody UserDTO userDTO) {
-        userService.createUser(userDTO);
-        return ResponseEntity.ok().build();
+        try {
+            userService.createUser(userDTO);
+            return ResponseEntity.ok().build();
+        } catch (EmailAlreadyUsedException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
     }
 
     @PostMapping("/login")
@@ -28,10 +37,10 @@ public class UserController {
 
         User user = userService.login(email, password);
         if (user != null) {
-            return ResponseEntity.ok().build();
+            String token = jwtUtil.generateToken(user.getEmail());
+            return ResponseEntity.ok(token); // 토큰 반환 확인
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 }
-
--- src/main/java/proj/travien/dto/UserDTO.java
+++ src/main/java/proj/travien/dto/UserDTO.java
@@ -1,13 +1,13 @@
 package proj.travien.dto;
 
-
 import lombok.Getter;
 import lombok.Setter;
+import lombok.AllArgsConstructor;
 
 @Getter
 @Setter
-    public class UserDTO {
-        private String email;
-        private String password;
-    }
-
+@AllArgsConstructor // 모든 매개변수를 받는 생성자 자동 생성
+public class UserDTO {
+    private String email;
+    private String password;
+}
