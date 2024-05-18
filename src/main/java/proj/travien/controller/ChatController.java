9f4c81c5f35fb9f501ec12385f7a1ba452ce818d
Darkroom01 <darkroom01@icloud.com>
2024-05-18 20:07:08 +0900

    실시간 채팅 구현 완료, websocket stomp 연결 성공, 채팅방 및 유저 인증 추가구현 필요

--- src/main/java/proj/travien/controller/ChatController.java
+++ src/main/java/proj/travien/controller/ChatController.java
@@ -3,13 +3,21 @@ package proj.travien.controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import proj.travien.domain.ChatMessage;
 
 @Controller
 public class ChatController {
 
-    @MessageMapping("/chat")
-    @SendTo("/topic/messages")
-    public String processMessage(String message) {
-        return message;
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(ChatMessage chatMessage) {
+        chatMessage.setType(ChatMessage.MessageType.JOIN);
+        return chatMessage;
     }
 }
