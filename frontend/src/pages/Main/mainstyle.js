24adebc8964b0b08d4dc8770a8ca547f3e21781a
UDyon <woo6142@naver.com>
2024-06-11 03:33:16 +0900

    메인 팝업 프로토타입 구현(러시아)

--- frontend/src/pages/Main/mainstyle.js
+++ frontend/src/pages/Main/mainstyle.js
@@ -10,3 +10,13 @@ export const Map=styled.div`
     fill: palevioletred;
   }
 `
+export const Popup = styled.div`
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.82);
+    border: 3px solid #33395b;
+    border-radius: 30px 0 30px 30px;
+    padding: 30px;
+    z-index: 1000;
+    width: 200px;
+    height: 200px;
+`;
