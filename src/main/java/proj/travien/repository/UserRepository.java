509296d75ba73035881ee1ccc9365ad3a49b7858
Darkroom01 <darkroom01@icloud.com>
2024-06-10 18:04:13 +0900

    채팅방 생성 로직 수정

--- src/main/java/proj/travien/repository/UserRepository.java
+++ src/main/java/proj/travien/repository/UserRepository.java
@@ -3,7 +3,8 @@ package proj.travien.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import proj.travien.domain.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserId(String username);
-    User findByNickname(String nickname);
+    Optional<User> findByNickname(String nickname);
 }
