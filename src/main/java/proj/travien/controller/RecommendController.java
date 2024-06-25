a5a88cc0abf9461449a466338830e491fa540e8d
yss9 <yse2196@gmail.com>
2024-06-26 00:47:25 +0900

    유사도 기반 추천 구현

--- src/main/java/proj/travien/controller/RecommendController.java
+++ src/main/java/proj/travien/controller/RecommendController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import proj.travien.domain.UserInfo;
 import proj.travien.dto.KeywordRequest;
 import proj.travien.dto.KeywordResultDTO;
 import proj.travien.service.KeywordRecommendService;
+import proj.travien.service.UserInfoRecommendationService;
 
 import java.util.List;
 
@@ -19,9 +21,17 @@ public class RecommendController {
     @Autowired
     private KeywordRecommendService keywordRecommendService;
 
+    @Autowired
+    private UserInfoRecommendationService userInfoRecommendationService;
+
     @PostMapping("/search")
     public List<KeywordResultDTO> search(@RequestBody KeywordRequest keywordRequest) {
         return keywordRecommendService.searchByKeywords(keywordRequest.getKeywords());
+    }
+
+    @PostMapping("/info-recommend")
+    public List<UserInfo> getRecommendations(@RequestBody UserInfo user){
+        return userInfoRecommendationService.getSimilarUsers(user);
     }
 
 }
