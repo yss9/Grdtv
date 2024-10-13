ffb85c068d503932776448b3d4f5b95c374b9e0e
Jin Naeun <121174014+jerxsluz@users.noreply.github.com>
2024-10-13 13:38:58 +0900

    댓글 구현 완

--- frontend/src/pages/reviewComment/list/list.js
+++ frontend/src/pages/reviewComment/list/list.js
@@ -1,34 +1,42 @@
-import { useMutation, useQuery } from "@apollo/client";
-import * as S from '../list/style'
-
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import * as S from "./style"; // 스타일 파일 import
+import { useParams } from "react-router-dom";
 
 export default function BoardCommentList() {
+    const { boardID } = useParams(); // URL에서 boardID 가져오기
+    const [comments, setComments] = useState([]); // 댓글 목록 상태
 
+    // 댓글 데이터 가져오기
+    const fetchComments = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8080/api/comments/${boardID}`);
+            setComments(response.data); // 댓글 목록 상태 업데이트
+        } catch (error) {
+            console.error("댓글 불러오기 실패:", error);
+        }
+    };
 
+    // 컴포넌트가 마운트될 때 댓글 가져오기
+    useEffect(() => {
+        fetchComments();
+    }, [boardID]);
 
     return (
-        <div>
-             {/*   <S.ItemWrapper>
-                    <S.FlexWrapper>
-                        <S.Avatar src="/images/avatar.png" />
-                        <S.MainWrapper>
-                            <S.WriterWrapper>
-                                <S.Writer>{writer}</S.Writer>
-                            </S.WriterWrapper>
-                            <S.Contents>{contents}</S.Contents>
-                        </S.MainWrapper>
-                        <S.OptionWrapper>
-                            <S.UpdateIcon src="/images/boardComment/list/option_update_icon.png/" />
-                            <S.DeleteIcon
-                                id={el._id}
-                                src="/images/boardComment/list/option_delete_icon.png/"
-                                onClick={onClickDelete}
-                            />
-                        </S.OptionWrapper>
-                    </S.FlexWrapper>
-                    <S.DateString>{getDate(createdAt)}</S.DateString>
-                </S.ItemWrapper>*/}
-        </div>
+        <S.CommentListWrapper>
+            {comments.length === 0 ? (
+                <S.NoComments>아직 댓글이 없습니다.</S.NoComments>
+            ) : (
+                comments.map((comment, index) => (
+                    <S.CommentItem key={index}>
+                        <S.CommentHeader>
+                            <S.Username>{comment.username}</S.Username>
+                            <S.CommentDate>{new Date(comment.createDate).toLocaleString()}</S.CommentDate>
+                        </S.CommentHeader>
+                        <S.CommentContent>{comment.content}</S.CommentContent>
+                    </S.CommentItem>
+                ))
+            )}
+        </S.CommentListWrapper>
     );
 }
