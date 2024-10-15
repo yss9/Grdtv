eb9aa51db79af89a6e782cc86dfbdcdaeba57787
Jin Naeun <121174014+jerxsluz@users.noreply.github.com>
2024-10-15 12:23:24 +0900

    나라 별 게시물 목록 가져오기

--- src/main/java/proj/travien/repository/PostRepository.java
+++ src/main/java/proj/travien/repository/PostRepository.java
@@ -3,5 +3,8 @@ package proj.travien.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import proj.travien.domain.Post;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByCountry(String country);
 }
